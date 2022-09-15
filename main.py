31e7d402a2efbdb256c7393b156a68907c67cdc4
Khiadrox <denniskoshta@gmail.com>
2022-09-15 12:35:41 -0700

    Replaced CartPole with StockPlot

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, render_template, flash, Response, make_response
-import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from yahoo_fin import stock_info as si
+
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+
+import io
 
 app = Flask(__name__)
+default_ticker = 'SPY'
+default_window = 356
 
 @app.route("/")
 def index():
@@ -19,13 +28,47 @@ def CartPole():
 def ArtCanvas():
     return render_template('ArtCanvas.html')
 
-# @app.route('/test')
-# def test():
-#     return render_template('test.html')
+@app.route('/StockPlot', methods=("POST", "GET"))
+def StockPlot():
+    global ticker, window
 
-# @app.route('/test2')
-# def test2():
-#     return render_template('test2.html')
+    if request.method == 'POST':
+        ticker = request.form.get('tickerInput')
+        window = int(request.form.get('windowInput'))
+
+    return render_template('StockPlot.html')
+
+@app.route('/plot.png', methods=['GET', 'POST'])
+def plot_png():
+    try:
+        fig = plot_stock_from_si(ticker, window)
+    except:
+        fig = plot_stock_from_si(default_ticker, default_window)
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+
+    return Response(output.getvalue(), mimetype='image/png')
+
+def plot_stock_from_si(ticker, window):
+    try:
+        df = si.get_data(ticker)
+    except:
+        fig, ax = plt.subplots(figsize=(16, 10))
+        plt.text(0.5, 0.5,'Sorry, this stock is not available right now, please try another.', horizontalalignment='center', verticalalignment='center', transform = ax.transAxes)
+        fig.suptitle(f"{ticker.upper()} was not found", fontsize=24)
+        return fig
+
+    fig, ax = plt.subplots(figsize=(16, 10))
+
+    plt.plot(df['adjclose'][-window:], c='b')
+
+    plt.xlabel("Date")
+    plt.ylabel("Price ($)")
+
+    fig.suptitle(f"{ticker.upper()} Stock Prices: Past {window} Days", fontsize=24)
+
+    return fig
 
 if __name__ == "__main__":
 	# start the flask app
