86a3d2a189e15284122ef537ad4e4dc9c5b422b7
dkoshta <dkoshta@ucsc.edu>
2023-04-25 00:34:38 -0700

    Fixed lots of Monkey's Paw stuff, gpt4 limiter actually maybe works now

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request, Response, jsonify, g
 from py_scripts import monkeys_paw
 import requests
 import os
-from collections import defaultdict
+# from collections import defaultdict
+from google.cloud import firestore
 
 environment = os.environ.get("ENVIRONMENT")
 
 app = Flask(__name__)
 
-user_api_calls = defaultdict(int)
+# user_api_calls = defaultdict(int)
+db = firestore.Client()
 
 @app.route("/")
 def index():
@@ -29,22 +31,61 @@ def MonkeysPaw():
 @app.route('/process_input', methods=['POST'])
 def process_input():
     user_input = request.form['user_input']
-    user_ip = request.remote_addr  # Get the user's IP address
 
-    # print(f"user_ip: {user_ip}")
-    # print(f"user_api_calls: {user_api_calls}")
+    # Get user IP
+    if 'X-Forwarded-For' in request.headers:
+        user_ip = request.headers['X-Forwarded-For']
+    else:
+        user_ip = request.remote_addr
 
     # Increment the GPT-4 API calls made by the user
-    user_api_calls[user_ip] += 1
+    user_doc_ref = db.collection('users').document(user_ip)
+    user_doc = user_doc_ref.get()
+
+    if user_doc.exists:
+        api_calls = user_doc.to_dict()['api_calls'] + 1
+    else:
+        api_calls = 1
+
+    if environment == "local":
+        print(f"user_ip: {user_ip}") 
+        print(f"user_api_calls: {api_calls}")
+
+    user_doc_ref.set({'api_calls': api_calls})
 
     # Check if the user has reached the limit for GPT-4 API calls
-    if user_api_calls[user_ip] > 3:
-        model = "gpt-3.5-turbo"
-    else:
+    if api_calls < 4 or (user_ip=="127.0.0.1" and environment=="local"):
         model = "gpt-4"
+    else:
+        model = "gpt-3.5-turbo"
 
     response = monkeys_paw.gpt_response(f"I wish {user_input}", model)
     return jsonify(response=response)
+
+@app.route('/get_current_model', methods=['GET'])
+def get_current_model():
+    # Get user IP
+    if 'X-Forwarded-For' in request.headers:
+        user_ip = request.headers['X-Forwarded-For']
+    else:
+        user_ip = request.remote_addr
+
+    # Increment the GPT-4 API calls made by the user
+    user_doc_ref = db.collection('users').document(user_ip)
+    user_doc = user_doc_ref.get()
+
+    if user_doc.exists:
+        api_calls = user_doc.to_dict()['api_calls']
+    else:
+        api_calls = 0
+
+    if api_calls < 4 or (user_ip=="127.0.0.1" and environment=="local"):
+        model = "gpt-4"
+    else:
+        model = "gpt-3.5-turbo"
+
+    return jsonify(model=model)
+
 
 # DEVELOPER CONSOLE DEBUG MESSAGE LOGGING (paste JS to desired html file and uncomment python)
 
--- static/js/MonkeysPaw.js
+++ static/js/MonkeysPaw.js
@@ -1,48 +1,38 @@
-let gpt4_api_call_count = 0;
-
-//Disable so that you can't submit nothing
+// Disable so that you can't submit nothing
 $("#submit_button").prop("disabled", true);
 
-$(document).ready(function() {
-    // Send response
-    $('#user-input-form').on('submit', function (event) {
-        $("#submit_button").prop("disabled", true);
-        event.preventDefault();
-        let userInput = $('#user_input').val();
-      
-        $("#response").html("");
-        $("#your_image").attr("src", "/static/assets/img/loading.gif");
-        // Show the "loading" text
-        $("#loading_text").show();
-      
-        $.ajax({
-          type: "POST",
-          url: "/process_input",
-          data: { user_input: userInput },
-          success: function (data) {
-            // Increment the GPT-4 API call count
-            gpt4_api_call_count += 1;
-      
-            // Check if the user has reached the GPT-4 API call limit
-            if (gpt4_api_call_count === 3) {
-              alert("You have reached the limit of GPT-4 API calls. Subsequent requests will use GPT-3.5-turbo.");
-            }
-      
-            $("#response").html(data.response);
-            $("#your_image").attr("src", "/static/assets/img/monkeys_paw.jpg");
-            $("#submit_button").prop("disabled", false);
-            // Hide the "loading" text
-            $("#loading_text").hide();
-          },
-          error: function () {
-            $("#response").html("An error occurred.");
-            $("#your_image").attr("src", "/static/assets/img/monkeys_paw.jpg");
-            $("#submit_button").prop("disabled", false);
-            // Hide the "loading" text
-            $("#loading_text").hide();
-          },
-        });
-    });
+updateCurrentModel();
+
+$('#user-input-form').on('submit', function (event) {
+  updateCurrentModel();
+  $("#submit_button").prop("disabled", true);
+  event.preventDefault();
+  let userInput = $('#user_input').val();
+
+  $("#response").html("");
+  $("#your_image").attr("src", "/static/assets/img/loading.gif");
+  // Show the "loading" text
+  $("#loading_text").show();
+
+  $.ajax({
+    type: "POST",
+    url: "/process_input",
+    data: { user_input: userInput },
+    success: function (data) {
+      $("#response").html(data.response);
+      $("#your_image").attr("src", "/static/assets/img/monkeys_paw.jpg");
+      $("#submit_button").prop("disabled", false);
+      // Hide the "loading" text
+      $("#loading_text").hide();
+    },
+    error: function () {
+      $("#response").html("An error occurred.");
+      $("#your_image").attr("src", "/static/assets/img/monkeys_paw.jpg");
+      $("#submit_button").prop("disabled", false);
+      // Hide the "loading" text
+      $("#loading_text").hide();
+    },
+  });
 });
 
 $(document).ready(function() {
@@ -67,3 +57,25 @@ $(document).ready(function() {
         $(this).width(Math.max(minInputWidth, inputLength * widthMultiplier));
     });
 });
+
+function updateCurrentModel() {
+    $.ajax({
+        type: "GET",
+        url: "/get_current_model",
+        success: function (data) {
+            // Set the GPT-4 API call count based on the returned model
+            gpt4_api_call_count = data.model === "gpt-4" ? gpt4_api_call_count : 3;
+
+            // Update the displayed model
+            $("#current_model").text(`Current Model: ${data.model}`);
+
+            // Alert user of reaching limit
+            if (gpt4_api_call_count === 3) {
+                alert("You have reached the limit of GPT-4 API calls. Subsequent requests will use GPT-3.5-turbo.");
+            }
+        },
+        error: function () {
+            $("#current_model").text("Error getting current model");
+        },
+    });
+}
